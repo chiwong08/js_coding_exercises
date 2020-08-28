83f6f8bb7543eb8fd2b2c0fc8e3eb8cf5ef2a257
chiwong08 <chiandwendy@gmail.com>
2020-08-28 15:50:45 +0100

    complete up to getMiddleCharacter in exercise001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,29 +1,40 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word[0].toUpperCase() + word.substring(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName[0].toUpperCase() + "." + lastName[0].toUpperCase();
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  return Math.round( ( originalPrice * ( 1 + (vatRate / 100))) *100 ) / 100 ;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  return Math.round((originalPrice * (1 - reduction/100 )) * 100 ) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  var midp;
+  var position;
+  var length;
+
+  if ( str.length % 2 == 1) {
+    position = str.length / 2;
+    length = 1;
+  } else {
+    position = str.length / 2 - 1;
+    length = 2;
+  }
+  return str.substring(position, position + length);
 }
 
 function reverseWord(word) {
