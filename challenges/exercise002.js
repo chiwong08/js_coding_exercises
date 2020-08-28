ed1c4496873e115ee36d84d102413c70c5a8e3e8
chiwong08 <chiandwendy@gmail.com>
2020-08-28 17:17:21 +0100

    completed exercise002.js coding

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,45 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if ( person.city === "Manchester" ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil( people / 40 );
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  var numSheep = 0;
+
+  for (var i = 0; i < arr.length; i++){
+    if ( arr[i] === "sheep") {
+      numSheep++;
+    }
+  }
+  return numSheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  var char1 = person.address.postCode[0];
+  var char2 = person.address.postCode[1];
+  if ( char1 === "M" ) {
+    if ( ( char2 >= 0 ) && ( char2 <= 9) ) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = {
