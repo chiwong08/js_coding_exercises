d10cf88c00a81c377232ffc3462d1679363efdf0
chiwong08 <chiandwendy@gmail.com>
2020-08-29 12:44:21 +0100

    Exercise005, done count1and0s

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,17 +1,47 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  
+  var found = false;
+
+  for ( var i = 0; i < nums.length; i++ ) {
+    if ( found === true ) {
+      return nums[i];
+    } else {
+      if ( n === nums[i]) {
+        found = true;
+        continue;
+      }
+    }
+  }
+  return null;
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  
+  var rObj = {};
+  rObj["0"] = 0;
+  rObj["1"] = 0;
+
+  for (var i = 0; i < str.length; i++ ) {
+    if ( str[i] == 0 ) {
+      rObj["0"]++;
+    } else if ( str[i] == 1 ) {
+      rObj["1"]++;
+    }   // technically we are just going to ignore anything else
+  }
+  return rObj;
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  var tmpString = n;
+  for (var i = 0; i < tmpString.length; i++ ){
+    console.log(tmpString[i]);
+  }
+
+  return tmpString;
 };
 
 const sumArrays = arrs => {
